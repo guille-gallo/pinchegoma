55fb870b9359822614b56eba395c9ab8eb031c2e
guille-gallo <guillermo.gallo@globant.com>
2017-09-06 10:20:06 -0300

    fragment para selector de cuadras agregado, falta meterle texto y selector de int

---
+++ app/src/main/java/guillermogallo/com/pinchegoma/Selector_cuadrasFragment.java
@@ -0,0 +1,25 @@
+package guillermogallo.com.pinchegoma;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Guille on 06/09/2017.
+ */
+
+public class Selector_cuadrasFragment extends Fragment {
+    /*@Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }*/
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.selector_cuadras, container, false);
+    }
+}
